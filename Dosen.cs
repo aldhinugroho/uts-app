beae77020e925052a57411a8bc1a711248f87069
aldhi <aldhinug@students.amikom.ac.id>
2022-05-18 15:32:14 +0700

    Tugas UTS 21.11.4431

---
+++ Dosen.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTS
+{
+    internal class Dosen
+    {
+        private int id { get; set; }
+        private string name { get; set; }
+        private int nik { get; set; }
+        private string gender { get; set; }
+        private string course { get; set; }
+
+        public Dosen(int id,string name, int nik, string gender, string course)
+        {
+            this.id = id;
+            this.name = name;
+            this.nik = nik;
+            this.gender = gender;
+            this.course = course;
+        }
+        public void cetakHasil()
+        {
+            Console.WriteLine("Data Default Dosen");
+            Console.WriteLine();
+            Console.WriteLine($"ID : {this.id}");
+            Console.WriteLine($"Name : {this.name}");
+            Console.WriteLine($"Gender : {this.gender}");
+            Console.WriteLine($"Course : {this.course}");
+        }
+
+        public void addDosen()
+        {
+            Console.Write("Inputkan Nama Dosen Terbaru: ");
+            this.name = Console.ReadLine();
+            Console.Write("Inputkan NIK Dosen Terbaru: ");
+            this.nik = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public string addCourse()
+        {
+            Console.Write($"Inputkan Course {this.name} : ");
+            this.course = Console.ReadLine();
+            return this.name;
+        }
+
+        public void cetakAkhir()
+        {
+            Console.WriteLine("Data Dosen Terbaru");
+            Console.WriteLine();
+            Console.WriteLine($"Name : {this.name}");
+            Console.WriteLine($"NIK : {this.nik}");
+            Console.WriteLine($"Course : {this.course}");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dosen dosen1 = new Dosen(1, "Aldhi Nugroho", 21114431, "Laki-laki", "Pemrograman");
+
+            dosen1.cetakHasil();
+            Console.WriteLine();
+            dosen1.addDosen();
+            Console.WriteLine();
+            dosen1.addCourse();
+            Console.WriteLine();
+            dosen1.cetakAkhir();
+
+            Console.ReadKey();
+        }
+    }
+}
